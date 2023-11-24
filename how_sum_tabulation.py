64b823bebedc266420b8c96ddcde54a53ef7889e
mArcinUci <mucinski@poczta.onet.pl>
2023-11-24 16:42:21 +0100

    Done, little different logic than i used before - in cooperation witch chatGPT :)

--- how_sum_tabulation.py
+++ how_sum_tabulation.py
@@ -1,17 +1,21 @@
-# goal is to check if you using values from nums can have 'target_sum' (you can use how many times you must those values)
+# goal is to check if you using values from nums can have 'target_sum' (all values can be used as many times as you wish) and what combination that would be (one is ok)
 
-def how_sum_tabulation(target_sum,nums):
-    array_of_sum_options = [[] for _ in range(target_sum+(max(nums)))]
-    for i in nums:
-        array_of_sum_options[i].append(i)
-        for number in nums:
-            if len(array_of_sum_options[i])>0:
-                array_of_sum_options[i+number].append([i,number])
-                               
-    return array_of_sum_options[target_sum] if len(array_of_sum_options[target_sum])>0 else None
-  
+def how_sum_tabulation(target_sum, nums):
+    array_of_sum_options = [None] * (target_sum + 1)
+    array_of_sum_options[0] = []
 
-print(how_sum_tabulation(7,[5,4,3]))
+    for i in range(target_sum + 1):
+        if array_of_sum_options[i] is not None:
+            for num in nums:
+                if i + num <= target_sum:
+                    new_sum_option = sorted(array_of_sum_options[i] + [num])
+                    if array_of_sum_options[i + num] is None or len(new_sum_option) < len(array_of_sum_options[i + num]):
+                        array_of_sum_options[i + num] = new_sum_option
+
+    return array_of_sum_options[target_sum]
+
+
+print(how_sum_tabulation(7,[5,1]))
 print(how_sum_tabulation(7,[2,3]))
 print(how_sum_tabulation(7,[2,4]))
-print(how_sum_tabulation(9,[5,4]))
+print(how_sum_tabulation(100,[15,5]))
